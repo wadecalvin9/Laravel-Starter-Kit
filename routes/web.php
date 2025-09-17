f9187ce15eb20ce0a869ed1312f7c9c54eb87cf9
wadecalvin9 <102851547+wadecalvin9@users.noreply.github.com>
2025-09-17 18:53:53 +0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+
+Route::get('/', function(){
+    return redirect('/user');
+
+});
+
+
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('profile.edit');
+    Volt::route('settings/password', 'settings.password')->name('password.edit');
+    Volt::route('settings/appearance', 'settings.appearance')->name('appearance.edit');
+});
+
+require __DIR__.'/auth.php';
